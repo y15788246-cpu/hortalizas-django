1551bfd7abee46e1b5bf839215f31a81859ef3f6
Kevin Tolentino <kevinbautista41832@gmail.com>
2026-01-19 01:03:01 -0600

    Fix ALLOWED_HOSTS for Render

--- hortalizas/settings.py
+++ hortalizas/settings.py
@@ -29,8 +29,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'hortalizas-django.onrender.com',
+]
 
 # Application definition
 
